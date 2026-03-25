e9946aa4c4daeff28d631cc011fda419e3fe51db
LucioArgiro <aleargiro3@gmail.com>
2026-03-25 20:43:27 -0300

    Fix: servicios de video

--- src/video/entities/video.entity.ts
+++ src/video/entities/video.entity.ts
@@ -1,6 +1,12 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { Categoria } from '../../categoria/entities/categoria.entity';
 
+export enum EstadoVideo {
+  PROCESANDO = 'PROCESANDO',
+  LISTO = 'LISTO',
+  ERROR = 'ERROR',
+}
+
 @Entity('videos')
 export class Video {
   @PrimaryGeneratedColumn('uuid')
@@ -8,29 +14,23 @@ export class Video {
 
   @Column()
   titulo: string;
+  @Column({ nullable: true })
+  assetId: string;
 
-  // 👇 ID de Mux para tareas de administración (como eliminar el video)
-  @Column()
-  assetId: string; 
-
-  // 👇 ID de Mux exclusivo para el reproductor del Frontend
-  @Column()
+  @Column({ nullable: true })
   playbackId: string;
 
-  // 👇 URL de Cloudinary para la portada del video (opcional al inicio)
+  @Column({ type: 'enum', enum: EstadoVideo, default: EstadoVideo.PROCESANDO })
+  estado: EstadoVideo;
+  
   @Column({ nullable: true })
   imagenUrl: string;
-
   @Column({ type: 'int' })
   orden: number;
-
   @Column({ type: 'int', nullable: true })
   duracion: number;
-
   @Column()
   idCategoria: string;
-
-  // Relación: Muchos videos pertenecen a una categoría
   @ManyToOne(() => Categoria, (categoria) => categoria.videos, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'idCategoria' })
   categoria: Categoria;
