d18aa7e1fd3c0406423e118dc5cbe36354d7706a
LucioArgiro <aleargiro3@gmail.com>
2026-04-19 13:22:29 -0300

    Fix: arreglos de seguridad en el back

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,10 +5,14 @@ import { AdminGuard } from './admin.guard';
 import { CreateUsuarioDto } from '../usuario/dto/create-usuario.dto';
 import { LoginDto } from './dto/login.dto';
 import { VerificarOtpDto } from './dto/verificar-otp.dto';
+import { CaptchaService } from './captcha.service'; 
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(
+    private readonly authService: AuthService,
+    private readonly captchaService: CaptchaService 
+  ) { }
 
   @Post('registro')
   async registrar(@Body() dto: CreateUsuarioDto) {
@@ -16,24 +20,28 @@ export class AuthController {
   }
 
   @Post('verificar-email')
-async verificarEmail(@Body() dto: VerificarOtpDto) {
-  return this.authService.verificarEmail(dto.correo, dto.codigo);
-}
+  async verificarEmail(@Body() dto: VerificarOtpDto) {
+    return this.authService.verificarEmail(dto.correo, dto.codigo);
+  }
 
   @Post('login')
   async login(@Body() dto: LoginDto, @Res({ passthrough: true }) res: Response) {
+    await this.captchaService.validarToken(dto.captchaToken); 
+    
     const { token, usuario } = await this.authService.login(dto.correo, dto.contrasena);
     const isProduction = process.env.NODE_ENV === 'production';
+    
     res.cookie('access_token', token, {
       httpOnly: true,
       secure: isProduction,
       sameSite: isProduction ? 'none' : 'lax',
       maxAge: 1000 * 60 * 60 * 24 * 1,
     });
-    return {usuario};
+    
+    return { usuario };
   }
 
-@Post('logout')
+  @Post('logout')
   logout(@Res({ passthrough: true }) res: Response) {
     const isProduction = process.env.NODE_ENV === 'production';
     res.clearCookie('access_token', {
